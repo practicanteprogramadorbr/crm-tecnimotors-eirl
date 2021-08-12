d020b8a978b4090b8f70fa017bc0131709ddae9e
Jefferson <jeffer_16_99@hotmail.com>
2021-08-12 01:25:20 -0500

    editar datos

--- src/app/mayoristas/form-m/form-m.component.ts
+++ src/app/mayoristas/form-m/form-m.component.ts
@@ -3,6 +3,7 @@ import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from
 import { ActivatedRoute, Router } from '@angular/router';
 import Departamento from 'src/app/interfaces/Departamento';
 import Distrito from 'src/app/interfaces/Distrito';
+import Mayorista from 'src/app/interfaces/Mayorista';
 import Provincia from 'src/app/interfaces/Provincia';
 import { ClienteService } from 'src/app/services/cliente.service';
 import { DireccionService } from 'src/app/services/direccion.service';
@@ -22,6 +23,8 @@ export class FormMComponent implements OnInit {
   distritos: Distrito[];
 
   loading: boolean = false;
+  isEditForm: boolean = false;
+  mayorista_a_actualizar: Mayorista;
 
   constructor(private fb: FormBuilder,
     private direccionService: DireccionService,
@@ -34,8 +37,25 @@ export class FormMComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.form = this.createForm();
-    this.listarDepartamentos();
+    this.activatedRoute.params.subscribe(params => {
+      this.form = this.createForm();
+      this.listarDepartamentos();
+      if (params.id) {
+        this.clienteService.buscarPorId(params.id).subscribe((cliente: any) => {
+          this.mayorista_a_actualizar = Object.assign({}, cliente);
+          delete cliente.id;
+          this.form.setValue(cliente);
+          this.isEditForm = true;
+        }, err => {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: err.error.details
+          });
+          this.go();
+        });
+      }
+    });
   }
 
   createForm() {
@@ -58,7 +78,8 @@ export class FormMComponent implements OnInit {
       fecha_registro: this.fb.control(null),
       ciudad: this.fb.control(null),
       comentario_reg_cliente: this.fb.control(null),
-      proteccion_datos: this.fb.control(true)
+      proteccion_datos: this.fb.control(true),
+      estado_id: this.fb.control(null)
     });
   }
 
@@ -99,12 +120,20 @@ export class FormMComponent implements OnInit {
     });
   }
 
-  registrar(e: Event) {
+  onSubmit(e: Event) {
     e.preventDefault();
     if (!this.form.valid) {
       this.form.markAllAsTouched();
       return;
     }
+    if (this.isEditForm) {
+      this.editar();
+    } else {
+      this.registrar();
+    }
+  }
+
+  registrar() {
     this.loading = true;
     this.clienteService.registrar(this.form.value).subscribe(cliente => {
       this.loading = false;
@@ -122,6 +151,27 @@ export class FormMComponent implements OnInit {
       );
       console.log(err);
     });
+  }
+
+  editar() {
+    this.loading = true;
+    this.clienteService.actualizarDatos(this.mayorista_a_actualizar.id, this.form.value)
+      .subscribe(cliente => {
+        this.loading = false;
+        Swal.fire({
+          icon: 'success',
+          title: 'Actualizado!',
+          text: `El cliente ${cliente.nombres} ${cliente.ap_materno} fue actualizado.`
+        });
+        this.router.navigateByUrl(this.return);
+      }, err => {
+        Swal.fire(
+          'Oops...',
+          'Ocurrio un error en la edición',
+          'error'
+        );
+        console.log(err);
+      });
   }
 
   getControl(name: string): AbstractControl {
